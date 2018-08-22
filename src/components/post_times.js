3a1f24a63a57b6a5dd7fa64a1a437675ee62ba82
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-08-21 23:09:38 -0500

    React in deep Routes, Lifecycle - creating Routes - Using Links in Routes

---
+++ src/components/post_times.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostTimes = () => {
+    return (
+        <div>
+            Post-Times
+        </div>
+    )
+}
+
+export default PostTimes;
---
+++ src/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+// COMPONENTS
+
+import Home from './components/home';
+import Posts from './components/posts';
+import Profile from './components/profile';
+// import Life from './components/lifecycles';
+// import Conditional from './components/conditional';
+
+
+// myawesomeapp.com/posts
+// myawesomeapp.com/profile/posts
+
+
+const App = () => {
+    return (
+        <BrowserRouter>
+            <div>
+                <header>
+                    <Link to="/">Home</Link><br/>
+                    <Link to="/posts">Posts</Link><br/>
+                    <Link to={{
+                        pathname:'profile',
+                        // hash: '#francis',
+                        // search: '?profile=true'
+                    }}>Profile</Link>
+                    <hr/>
+                </header>
+                <Route path="/" exact component={Home}/>
+                <Route path="/posts" component={Posts} />
+                <Route path="/profile" component={Profile} />
+                {/* <Route path="/lifecycles" component={Life} />
+                <Route path="/conditional" component={Conditional} /> */}
+            </div>
+        </BrowserRouter>
+    )
+}
+
+
+ReactDom.render(
+    <App />,
+    document.querySelector('#root')
+)

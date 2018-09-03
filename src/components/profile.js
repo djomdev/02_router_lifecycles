b688afbd6040db76544c17fa144b43c5aeecd167
Diego Ortega <diegodeortega@MacBook-Pro-de-Diego.local>
2018-09-03 16:23:03 -0500

    HOC - High Order Commponents

--- src/components/profile.js
+++ src/components/profile.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Card from '../hoc/card';
+import Auth from '../hoc/auth';
 
 const Profile = (props) => {
 
     const redir = () => {
-        //props.history.push('/')
-    }
+        // props.history.push('/')
+    } 
     return (
-        <Card>
-            <Link to={{
-                pathname: `${props.match.url}/posts`
-            }}>take me to /profile/posts</Link>
-
-            {redir()}
-        </Card>
+        <Auth>
+            <Card>
+                <Link to={{
+                    pathname: `${props.match.url}/posts`
+                }}>take me to /profile/posts</Link>
+                {/* {redir()} */}
+            </Card>
+        </Auth>
     )
 }
 
